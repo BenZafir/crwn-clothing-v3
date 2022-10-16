67683a6bc0e2e219a5d01df2068072150f2929af
ben <ben@gmail.com>
2022-10-16 17:33:26 +0300

    first commit

---
+++ src/routes/shop/shop.component.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../category/category.component';
+import { fetchCategoriesStartAsync } from '../../store/categories/category.action';
+
+const Shop = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCategoriesStartAsync());
+  }, []);
+
+  return (
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=':category' element={<Category />} />
+    </Routes>
+  );
+};
+
+export default Shop;
